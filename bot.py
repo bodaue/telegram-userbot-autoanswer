abc873ceaf0b4b081542661393e9089781a16ea2
bodaue <timka227@mail.ru>
2023-05-09 22:40:01 +0300

    Bug fixes

--- bot.py
+++ bot.py
@@ -1,11 +1,11 @@
 from pyrogram import Client, idle
 
-from config import config
+from userbot.config import config
 
 app = Client(api_id=config.bot.api_id,
              api_hash=config.bot.api_hash,
              name='my_account',
-             plugins=dict(root='handlers'))
+             plugins=dict(root='userbot'))
 
 if __name__ == '__main__':
     app.start()
